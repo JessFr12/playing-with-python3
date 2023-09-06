4b8d5434f9a5456b09c32304ea0833e63f532925
Jesse Franco <108185010+JessFr12@users.noreply.github.com>
2023-09-05 18:38:48 -0600

    Insert try, except, and finally clause for error handling

--- intro-to-py.py
+++ intro-to-py.py
@@ -1,13 +1,19 @@
 # Input your first name, last name, age, and gender
-first_name = input("First Name: ")
-last_name = input("Last Name: ")
-age = int(input("Age: "))
-gender = input("M or F? ")
-correct = True
 # Create message to confirm information
-if gender.lower == "m":
-    print("Hello Mr." + last_name + "! Thank you for your info.\n"
-        + first_name + "\n" + last_name + "\n" + str(age) + "\n" + gender)
-else:
-    print("Hello Ms." + last_name + "! Thank you for your info.\n"
-        + first_name + "\n" + last_name + "\n" + str(age) + "\n" + gender)
+try:
+    first_name = input("First Name: ")
+    last_name = input("Last Name: ")
+    age = int(input("Age: "))
+    gender = input("M or F? ")
+    correct = True
+    if gender == "m" or gender == "M":
+        print("Hello Mr." + last_name + "! Thank you for your info.\n"
+            + first_name + "\n" + last_name + "\n" + str(age) + "\n" + gender)
+    else:
+        print("Hello Ms." + last_name + "! Thank you for your info.\n"
+            + first_name + "\n" + last_name + "\n" + str(age) + "\n" + gender)
+except:
+    print("Name must contain only letters, age must be only integers, "
+          + "and gender should only contain characters (M or F)")
+finally:
+    print("Please try again as you please!")
