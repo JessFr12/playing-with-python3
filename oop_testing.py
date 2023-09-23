4acbe10fb9e2949030c4ec603073985fc592397b
Jesse Franco <108185010+JessFr12@users.noreply.github.com>
2023-09-22 23:40:00 -0500

    Create Dog class

---
+++ oop_testing.py
@@ -0,0 +1,12 @@
+class Dog:
+    def __init__(self, name, color, behavior):
+        self.name = name
+        self.color = color
+        self.behavior = behavior
+    
+    def bark():
+        print("ruff ruuff...arf")
+
+george = Dog("George", "Gold", "Good boy")
+Dog.bark()
+
