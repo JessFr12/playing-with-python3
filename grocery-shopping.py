19bd7e0f34e98f07dc136c8fdc07fe14a5791988
Jesse Franco <108185010+JessFr12@users.noreply.github.com>
2023-09-26 18:55:39 -0500

    Print better layout for items

--- grocery-shopping.py
+++ grocery-shopping.py
@@ -6,7 +6,10 @@ print("Hello! Welcome to our Market, how are you doing? ")
 input()
 print("Well we are so happy that you are here!\n"
       + "Please take a look at our selection and let us know if you " 
-      + "need any help.\n\n")
+      + "need any help.\n")
 
 food = ["Milk", "Apple juice", "Bananas", "Bread", "Potato Chips", "Cookies", "Beef"]
-print(food)
+def print_menu():
+    for menu in food:
+        print(menu)
+print_menu()
