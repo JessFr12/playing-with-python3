4e60fcf97e9c963420a324b86b07403aae543dc4
Jesse Franco <108185010+JessFr12@users.noreply.github.com>
2023-09-29 21:34:23 -0500

    Update list to key: value format
    Added price to store items

--- grocery-shopping.py
+++ grocery-shopping.py
@@ -8,8 +8,11 @@ print("Well we are so happy that you are here!\n"
       + "Please take a look at our selection and let us know if you " 
       + "need any help.\n")
 
-food = ["Milk", "Apple juice", "Bananas", "Bread", "Potato Chips", "Cookies", "Beef"]
+food = {
+    "Milk": 3.29, "Apple juice": 2.99, "Bananas": 3.00, "Bread": 2.50,
+      "Potato Chips": 1.50, "Cookies": 2.40, "Beef": 4.00
+}
 def print_menu():
-    for menu in food:
-        print(menu)
+    for key, value in food.items():
+        print(f"{key}: {value}")
 print_menu()
