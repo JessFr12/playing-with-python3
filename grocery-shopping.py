62025b9acf9dcdea37472e63c5d9051853d2d5a6
Jesse Franco <108185010+JessFr12@users.noreply.github.com>
2023-10-18 22:37:41 -0500

    Prompt user for items until they are finished

--- grocery-shopping.py
+++ grocery-shopping.py
@@ -16,3 +16,21 @@ def print_menu():
     for key, value in food.items():
         print(f"{key}: {value}")
 print_menu()
+# list to store user's selection/s
+user_selection = []
+# Keep prompting the user for input until they are finished
+while True:
+    user_input = input("\nEnter an item (type 'done' when finished):\n")
+
+    if user_input == "done": 
+        break
+
+    price = food.get(user_input, None)
+
+    if price is None:
+        print("Item is not available in store.")
+        continue
+
+    user_selection.append(user_input)
+
+
